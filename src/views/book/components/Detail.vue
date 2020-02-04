4163f12fa89f673b47de01ec45d26b2224e7be4e
Wang Pengyue <wangpengyue@liuyuandeMacBook-Pro.local>
2020-02-04 15:02:18 +0800

    add form

--- src/views/book/components/Detail.vue
+++ src/views/book/components/Detail.vue
@@ -1,12 +1,47 @@
 <template>
   <div class="detail">
-    isEdit: {{ isEdit }}
+    <el-form ref="postForm">
+      <sticky :class-name="'sub-navbar'">
+        <el-button v-if="isEdit" @click="showGuide">显示帮助</el-button>
+        <el-button v-loading="loading" type="success" @click="submitForm">{{ isEdit ? '编辑电子书' : '新增电子书' }}</el-button>
+      </sticky>
+      <div class="detail-container">
+        <el-row>
+          <warning />
+          <el-col :span="24" />
+          <el-col />
+        </el-row>
+      </div>
+    </el-form>
   </div>
 </template>
 <script>
+
+import Sticky from '../../../components/Sticky/index'
+import Warning from './Warning'
 export default {
+  components: { Sticky, Warning },
   props: {
     isEdit: Boolean
+  },
+  data() {
+    return {
+      loading: false,
+      postForm: {
+        status: 'draft'
+      }
+    }
+  },
+  methods: {
+    submitForm() {
+      this.loading = true
+      setTimeout(() => {
+        this.loading = false
+      }, 1000)
+    },
+    showGuide() {
+      console.log('show guide ...')
+    }
   }
 }
 </script>
