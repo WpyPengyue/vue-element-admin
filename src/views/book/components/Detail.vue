64277af2d5e84195b5fb6ff4456cf9edb33c783c
Wang Pengyue <wangpengyue@liuyuandeMacBook-Pro.local>
2020-03-06 17:13:07 +0800

    delete exist ebook

--- src/views/book/components/Detail.vue
+++ src/views/book/components/Detail.vue
@@ -1,133 +1,152 @@
 <template>
-  <div class="detail">
-    <el-form ref="postForm" :model="postForm" :rules="rules">
-      <sticky :class-name="'sub-navbar'">
-        <el-button v-if="isEdit" @click="showGuide">显示帮助</el-button>
-        <el-button v-loading="loading" type="success" @click="submitForm">{{ isEdit ? '编辑电子书' : '新增电子书' }}</el-button>
-      </sticky>
-      <div class="detail-container">
-        <el-row>
-          <warning />
-          <el-col :span="24">
-            <ebook-upload
-              :file-list="fileList"
-              :disabled="isEdit"
-              @onSuccess="onUploadSuccess"
-              @onRemove="onUploadRemove"
-            />
-          </el-col>
-          <el-col :span="24">
-            <el-form-item prop="title">
-              <MdInput v-model="postForm.title" :maxlength="100" name="name" required>
-                书名
-              </MdInput>
-            </el-form-item>
-          </el-col>
-        </el-row>
-        <el-row>
-          <el-col :span="12">
-            <el-form-item prop="author" label="作者:" :label-width="labelWidth">
-              <el-input v-model="postForm.author" placeholder="作者" />
-            </el-form-item>
-          </el-col>
-          <el-col :span="12">
-            <el-form-item prop="publisher" label="出版社:" :label-width="labelWidth">
-              <el-input v-model="postForm.publisher" placeholder="出版社" />
-            </el-form-item>
-          </el-col>
-        </el-row>
-        <el-row>
-          <el-col :span="12">
-            <el-form-item prop="language" label="语言:" :label-width="labelWidth">
-              <el-input v-model="postForm.language" placeholder="语言" />
-            </el-form-item>
-          </el-col>
-          <el-col :span="12">
-            <el-form-item prop="rootFile" label="根文件:" :label-width="labelWidth">
-              <el-input v-model="postForm.rootFile" placeholder="根文件" disabled />
-            </el-form-item>
-          </el-col>
-        </el-row>
-        <el-row>
-          <el-col :span="12">
-            <el-form-item label="文件路径:" :label-width="labelWidth">
-              <el-input v-model="postForm.filePath" placeholder="文件路径" disabled />
-            </el-form-item>
-          </el-col>
-          <el-col :span="12">
-            <el-form-item label="解压路径:" :label-width="labelWidth">
-              <el-input v-model="postForm.unzipPath" placeholder="解压路径" disabled />
-            </el-form-item>
-          </el-col>
-        </el-row>
-        <el-row>
-          <el-col :span="12">
-            <el-form-item label="封面路径:" :label-width="labelWidth">
-              <el-input v-model="postForm.filePath" placeholder="文件路径" disabled />
-            </el-form-item>
-          </el-col>
-          <el-col :span="12">
-            <el-form-item label="文件名称:" :label-width="labelWidth">
-              <el-input v-model="postForm.unzipPath" placeholder="解压路径" disabled />
-            </el-form-item>
-          </el-col>
-        </el-row>
-        <el-row>
-          <el-col :span="24">
-            <el-form-item label="封面图片:" :label-width="labelWidth">
-              <a v-if="postForm.cover" :href="postForm.cover" target="_blank">
-                <img :src="postForm.cover" class="preview-img">
-              </a>
-              <span v-else>无</span>
-            </el-form-item>
-          </el-col>
-        </el-row>
-        <el-row>
-          <el-col :span="24">
-            <el-form-item :label-width="labelWidth" label="目录:">
-              <div v-if="postForm.contents && postForm.contents.length > 0" class="contents-wrapper">
-                <el-tree :data="contentsTree" @node-click="onContentlick" />
-              </div>
-              <span v-else>无</span>
-            </el-form-item>
-          </el-col>
-        </el-row>
-      </div>
-    </el-form>
-  </div>
+  <el-form ref="postForm" :model="postForm" :rules="rules">
+    <sticky :class-name="'sub-navbar'">
+      <el-button v-if="!isEdit" @click="showGuide">显示帮助</el-button>
+      <el-button
+        v-loading="loading"
+        type="success"
+        style="margin-left: 10px"
+        @click="submitForm"
+      >
+        {{ isEdit ? '编辑电子书' : '新增电子书' }}
+      </el-button>
+    </sticky>
+    <div class="detail-container">
+      <el-row>
+        <warning />
+        <el-col :span="24">
+          <ebook-upload
+            :file-list="fileList"
+            :disabled="isEdit"
+            @onSuccess="onUploadSuccess"
+            @onRemove="onUploadRemove"
+          />
+        </el-col>
+        <el-col :span="24">
+          <el-form-item prop="title">
+            <MdInput v-model="postForm.title" :maxlength="100" name="name" required>
+              书名
+            </MdInput>
+          </el-form-item>
+          <el-row>
+            <el-col :span="12">
+              <el-form-item prop="author" label="作者：" :label-width="labelWidth">
+                <el-input
+                  v-model="postForm.author"
+                  placeholder="作者"
+                />
+              </el-form-item>
+            </el-col>
+            <el-col :span="12">
+              <el-form-item prop="publisher" label="出版社：" :label-width="labelWidth">
+                <el-input
+                  v-model="postForm.publisher"
+                  placeholder="出版社"
+                />
+              </el-form-item>
+            </el-col>
+          </el-row>
+          <el-row>
+            <el-col :span="12">
+              <el-form-item prop="language" label="语言：" :label-width="labelWidth">
+                <el-input
+                  v-model="postForm.language"
+                  placeholder="语言"
+                />
+              </el-form-item>
+            </el-col>
+            <el-col :span="12">
+              <el-form-item prop="rootFile" label="根文件：" :label-width="labelWidth">
+                <el-input
+                  v-model="postForm.rootFile"
+                  placeholder="根文件"
+                  disabled
+                />
+              </el-form-item>
+            </el-col>
+          </el-row>
+          <el-row>
+            <el-col :span="12">
+              <el-form-item prop="filePath" label="文件路径：" :label-width="labelWidth">
+                <el-input
+                  v-model="postForm.filePath"
+                  placeholder="文件路径"
+                  disabled
+                />
+              </el-form-item>
+            </el-col>
+            <el-col :span="12">
+              <el-form-item prop="unzipPath" label="解压路径：" :label-width="labelWidth">
+                <el-input
+                  v-model="postForm.unzipPath"
+                  placeholder="解压路径"
+                  disabled
+                />
+              </el-form-item>
+            </el-col>
+          </el-row>
+          <el-row>
+            <el-col :span="12">
+              <el-form-item prop="coverPath" label="封面路径：" :label-width="labelWidth">
+                <el-input
+                  v-model="postForm.coverPath"
+                  placeholder="封面路径"
+                  disabled
+                />
+              </el-form-item>
+            </el-col>
+            <el-col :span="12">
+              <el-form-item prop="originalName" label="文件名称：" :label-width="labelWidth">
+                <el-input
+                  v-model="postForm.originalName"
+                  placeholder="文件名称"
+                  disabled
+                />
+              </el-form-item>
+            </el-col>
+          </el-row>
+          <el-row>
+            <el-col :span="24">
+              <el-form-item prop="cover" label="封面：" :label-width="labelWidth">
+                <a v-if="postForm.cover" :href="postForm.cover" target="_blank">
+                  <img :src="postForm.cover" class="preview-img">
+                </a>
+                <span v-else>无</span>
+              </el-form-item>
+            </el-col>
+          </el-row>
+          <el-row>
+            <el-col :span="24">
+              <el-form-item label="目录：" :label-width="labelWidth">
+                <div v-if="contentsTree && contentsTree.length > 0" class="contents-wrapper">
+                  <el-tree :data="contentsTree" @node-click="onContentClick" />
+                </div>
+                <span v-else>无</span>
+              </el-form-item>
+            </el-col>
+          </el-row>
+        </el-col>
+      </el-row>
+    </div>
+  </el-form>
 </template>
 <script>
 
-import Sticky from '../../../components/Sticky/index'
-import EbookUpload from '../../../components/EbookUpload'
+import Sticky from '../../../components/Sticky'
 import Warning from './Warning'
-import MdInput from '../../../components/MDinput/index'
-import { createBook } from '../../../api/book'
-
-const defaultForm = {
-  title: '',
-  author: '',
-  publisher: '',
-  language: '',
-  rootFile: '',
-  cover: '',
-  url: '',
-  originalName: '',
-  fileName: '',
-  coverPath: '',
-  filePath: '',
-  unzipPath: ''
-}
+import EbookUpload from '../../../components/EbookUpload'
+import MdInput from '../../../components/MDinput'
+import { createBook, getBook, updateBook } from '../../../api/book'
 
 const fields = {
-  title: '标题',
+  title: '书名',
   author: '作者',
-  language: '语言',
-  publisher: '出版社'
+  publisher: '出版社',
+  language: '语言'
 }
 
 export default {
-  components: { Sticky, Warning, EbookUpload, MdInput },
+  components: { MdInput, Sticky, Warning, EbookUpload },
   props: {
     isEdit: Boolean
   },
@@ -141,9 +160,7 @@ export default {
     }
     return {
       loading: false,
-      postForm: {
-        status: 'draft'
-      },
+      postForm: {},
       fileList: [],
       labelWidth: '120px',
       contentsTree: [],
@@ -155,40 +172,29 @@ export default {
       }
     }
   },
+  created() {
+    if (this.isEdit) {
+      const fileName = this.$route.params.fileName
+      this.getBookData(fileName)
+    }
+  },
   methods: {
-    onContentlick(data) {
+    getBookData(fileName) {
+      getBook(fileName).then(response => {
+        this.setData(response.data)
+      })
+    },
+    onContentClick(data) {
+      console.log(data)
       if (data.text) {
         window.open(data.text)
       }
     },
     setDefault() {
-      this.postForm = Object.assign({}, defaultForm)
-    },
-    submitForm() {
-      if (!this.loading) {
-        this.loading = true
-        this.$refs.postForm.validate((valid, fields) => {
-          // console.log('validate', valid, fields)
-          if (valid) {
-            const book = Object.assign({}, this.postForm)
-            delete book.contents
-            delete book.contentsTree
-            if (this.isEdit) {
-              // updateBook()
-            } else {
-              createBook(book)
-            }
-            console.log('book', book)
-          } else {
-            const message = fields[Object.keys(fields)[0]][0].message
-            this.$message({ message, type: 'error' })
-            this.loading = false
-          }
-        })
-      }
-    },
-    showGuide() {
-      console.log('show guide ...')
+      // this.postForm = Object.assign({}, defaultForm)
+      this.contentsTree = []
+      this.fileList = []
+      this.$refs.postForm.resetFields()
     },
     setData(data) {
       const {
@@ -225,22 +231,67 @@ export default {
         unzipPath
       }
       this.contentsTree = contentsTree
-      console.log('postForm', this.postForm)
+      this.fileList = [{ name: originalName || fileName, url }]
     },
     onUploadSuccess(data) {
       console.log('onUploadSuccess', data)
       this.setData(data)
     },
     onUploadRemove() {
-      console.log('onUploadRemove')
       this.setDefault()
+    },
+    submitForm() {
+      const onSuccess = (response) => {
+        const { msg } = response
+        this.$notify({
+          title: '操作成功',
+          message: msg,
+          type: 'success',
+          duration: 2000
+        })
+        this.loading = false
+      }
+
+      if (!this.loading) {
+        this.loading = true
+        this.$refs.postForm.validate((valid, fields) => {
+          console.log(valid, fields)
+          if (valid) {
+            const book = Object.assign({}, this.postForm)
+            delete book.contentsTree
+            if (!this.isEdit) {
+              createBook(book).then(response => {
+                onSuccess(response)
+                this.setDefault()
+              }).catch(() => {
+                this.loading = false
+              })
+            } else {
+              updateBook(book).then(response => {
+                onSuccess(response)
+              }).catch(() => {
+                this.loading = false
+              })
+            }
+          } else {
+            const message = fields[Object.keys(fields)[0]][0].message
+            this.$message({ message, type: 'error' })
+            this.loading = false
+          }
+        })
+      }
+    },
+    showGuide() {
+      console.log('show guide...')
     }
   }
 }
 </script>
+
 <style lang="scss" scoped>
   .detail-container {
     padding: 40px 50px 20px;
+
     .preview-img {
       width: 200px;
       height: 270px;
